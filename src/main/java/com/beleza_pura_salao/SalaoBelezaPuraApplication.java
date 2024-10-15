6031bc60a85ae7ab5a0db08bc33dc7a0e0bbaac0
marcelojacint <marcelojacintoferreirq@gmail.com>
2024-10-15 15:21:19 -0300

    projeto criado

---
+++ src/main/java/com/beleza_pura_salao/SalaoBelezaPuraApplication.java
@@ -0,0 +1,13 @@
+package com.beleza_pura_salao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoBelezaPuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoBelezaPuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beleza_pura_salao/SalaoBelezaPuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beleza_pura_salao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoBelezaPuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
